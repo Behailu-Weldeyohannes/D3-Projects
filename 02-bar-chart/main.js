62b07476fc3e8759764f607fc9b3542b45603707
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2023-02-06 00:34:37 -0500

    label added

--- 02-bar-chart/main.js
+++ 02-bar-chart/main.js
@@ -5,7 +5,6 @@ var svgWidth = 500,
   svgHeight = 300,
   barPadding = 5;
 var barWidth = svgWidth / dataset.length;
-
 var svg = d3.select("svg").attr("width", svgWidth).attr("height", svgHeight);
 
 var barChart = svg
@@ -20,7 +19,24 @@ var barChart = svg
     return d;
   })
   .attr("width", barWidth - barPadding)
+  .attr("class", "bar")
   .attr("transform", function (d, i) {
     var translate = [barWidth * i, 0];
     return "translate(" + translate + ")";
   });
+
+var text = svg
+  .selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function (d) {
+    return d;
+  })
+  .attr("y", function (d, i) {
+    return svgHeight - d - 2;
+  })
+  .attr("x", function (d, i) {
+    return barWidth * i;
+  })
+  .attr("fill", "#A64C38");
